7e7d3216c8b0200da3082be029e48fac39fa4c29
Atul Varma <varmaa@gmail.com>
2012-07-27 10:29:09 -0500

    added a bunch of methods to Git

--- git.js
+++ git.js
@@ -1,21 +1,69 @@
 var spawn = require('child_process').spawn,
+    path = require('path'),
+    fs = require('fs'),
     CommandSerializer = require('./command-serializer');
 
 function Git(options) {
   var executable = options.executable || 'git',
       rootDir = options.rootDir,
-      commands = CommandSerializer(),
+      debug = options.debug,
+      cs = CommandSerializer(),
       self = {};
   
   function git(args, cb) {
+    var cmdline = 'git ' + args.join(' ');
+    if (!cb)
+      throw new Error('no callback given for: ' + cmdline);
     var process = spawn(executable, args, {cwd: rootDir});
+    process.stderr.setEncoding('utf8');
+    
+    if (debug)
+      process.stderr.on('data', function(chunk) {
+        console.warn(cmdline + ': ' + chunk);
+      });
+    
     process.on('exit', function(code) {
-      cb(code || null);
+      cb(code ? 'subprocess failed: ' + cmdline : null);
     });
   }
   
-  self.init = commands.serialized(function(cb) {
-    git(['init'], cb);
+  var abspath = self.abspath = function abspath(filename) {
+    return path.join(rootDir, filename);
+  }
+  
+  var commands = {
+    init: function(cb) {
+      git(['init'], cb);
+    },
+    reset: function(cb) {
+      git(['reset', '--hard'], cb);
+    },
+    commit: function(options, cb) {
+      git(['commit', '--author=' + options.author,
+           '-m', options.message], cb);
+    },
+    revert: function(cb) {
+      git(['revert', 'HEAD'], cb);
+    },
+    addFile: function(filename, data, cb) {
+      var self = this;
+      fs.writeFile(abspath(filename), data, function(err) {
+        if (err) return cb(err);
+        self.add([filename], cb);
+      });
+    },
+    add: function(filenames, cb) {
+      git(['add'].concat(filenames), cb);
+    },
+    rm: function(filenames, cb) {
+      git(['rm', '-f'].concat(filenames), cb);
+    }
+  };
+  
+  Object.keys(commands).forEach(function(name) {
+    self[name] = cs.serialized(function() {
+      return commands[name].apply(commands, arguments);
+    });
   });
 
   return self;
--- test/test-git.js
+++ test/test-git.js
@@ -1,28 +1,59 @@
 var expect = require('expect.js'),
     fs = require('fs'),
+    path = require('path'),
     exec =  require('child_process').exec,
     Git = require('../git');
 
 describe('Git', function() {
-  it('.init() should work', function(done) {
-    var rootDir = __dirname + '/../_testrepo',
-        git = Git({rootDir: rootDir});
-
-    function go() {
-      fs.mkdirSync(rootDir);
-      git.init(function(err) {
-        expect(err).to.be(null);
-        expect(fs.existsSync(rootDir + '/.git')).to.be(true);
-        done();
-      });
-    }
-    
+  var rootDir = __dirname + '/../_testrepo';
+  
+  function nukeRootDir(cb) {
     if (fs.existsSync(rootDir))
-      exec('rm -rf ' + rootDir, function(err) {
-        if (err) throw err;
-        go();
-      });
+      exec('rm -rf ' + rootDir, cb);
     else
-      go();
+      cb(null);
+  }
+  
+  beforeEach(function(done) {
+    nukeRootDir(function(err) {
+      if (err) throw err;
+      fs.mkdirSync(rootDir);
+      done();
+    });
+  });
+  
+  afterEach(nukeRootDir);
+  
+  it('init() should work', function(done) {
+    var git = Git({rootDir: rootDir, debug: true});
+
+    git.init(function(err) {
+      expect(err).to.be(null);
+      expect(fs.existsSync(git.abspath('.git'))).to.be(true);
+      done();
+    });
+  });
+  
+  it('revert() should work', function(done) {
+    var git = Git({rootDir: rootDir, debug: true});
+    
+    git.init()
+      .addFile('blah.txt', 'hello there')
+      .commit({author: 'Foo <foo@foo.org>', message: 'origination.'})
+      .addFile('blah.txt', 'goodbye yo')
+      .commit({
+        author: 'Foo <foo@foo.org>',
+        message: 'changed file.'
+      }, function(err) {
+        if (err) return done(err);
+        expect(fs.readFileSync(git.abspath('blah.txt'), 'utf8'))
+          .to.be('goodbye yo');
+        git.revert(function(err) {
+          if (err) return done(err);
+          expect(fs.readFileSync(git.abspath('blah.txt'), 'utf8'))
+            .to.be('hello there');
+          done();
+        });
+      });
   });
 });
